8d60a68399910518969afafbbf972de68b6a609b
Alexander Sosedkin <monk@unboiled.info>
2021-05-24 01:13:56 +0200

    update 1, 8+4+2 keys, all letters placed

--- chordkeyboard/main.py
+++ chordkeyboard/main.py
@@ -46,67 +46,149 @@ def type_(*codes):
     return press_release
 
 
+def shifted(code):
+    def press_release(ui):
+        ui.write(ec.EV_KEY, ec.KEY_LEFTSHIFT, 1)
+        ui.write(ec.EV_KEY, code, 1)
+        ui.write(ec.EV_KEY, code, 0)
+        ui.write(ec.EV_KEY, ec.KEY_LEFTSHIFT, 0)
+        ui.syn()
+    return press_release
+
+
 RULES = (
-    # a-rocks
-    ('a press  r press  r release', 'a defused', type_(ec.KEY_A,
-                                                       ec.KEY_L)),
+    # a rocks
+    ('a (press|defused)  r press  r release', 'a defused',
+     type_(ec.KEY_A, ec.KEY_L)),
+    ('a (press|defused)  s press  s release', 'a defused',
+     type_(ec.KEY_Q)),
+    ('a (press|defused)  t press  t release', 'a defused',
+     type_(ec.KEY_X)),
+    ('a (press|defused)  e press  e release', 'a defused',
+     type_(ec.KEY_Q, ec.KEY_U)),
+    # end of a
     ('a defused (.*?) a release', r'\1', None),
 
-    # r-rocks
+    # r rocks
+    ('r (press|defused)  a press  a release', 'r defused',
+     type_(ec.KEY_V)),
     ('r (press|defused)  s press  s release', 'r defused',
-     type_(ec.KEY_C)),
+     type_(ec.KEY_P)),
     ('r (press|defused)  t press  t release', 'r defused',
+     type_(ec.KEY_G)),
+    # r same-finger
+    ('r (press|defused)  down press', 'r defused  down inactive', None),
+    ('r defused  down inactive (.*) (r|down) repeat',
+     r'r defused  down inactive \1', None),
+    # r same-finger rock
+    ('r defused  down inactive  down release', 'r defused',
+     type_(ec.KEY_L)),
+    # r same-finger roll
+    ('r defused  down inactive  r release', 'down inactive',
      type_(ec.KEY_W)),
-    ('r (press|defused)  o press  o release', 'r defused',
-     type_(ec.KEY_C, ec.KEY_O)),
+    # end of r
     ('r defused (.*?) r release', r'\1', None),
 
-    # s-rocks
+    # s rocks
+    ('s (press|defused)  a press  a release', 's defused',
+     type_(ec.KEY_F)),
+    ('s (press|defused)  r press  r release', 's defused',
+     type_(ec.KEY_M)),
     ('s (press|defused)  t press  t release', 's defused',
      type_(ec.KEY_D)),
-    ('s (press|defused)  r press  r release', 's defused',
-     type_(ec.KEY_L)),
-    ('s (press|defused)  up press', 's defused  up inactive',
+    ('s (press|defused)  o press  o release', 's defused',
+     type_(ec.KEY_C, ec.KEY_O)),
+    # s same-finger
+    ('s (press|defused)  up press', 's defused  up inactive', None),
+    ('s defused  up inactive (.*) (s|up) repeat',
+     r's defused  up inactive \1', None),
+    # s same-finger rock
+    ('s defused  up inactive  up release', 's defused',
+     type_(ec.KEY_C)),
+    # s same-finger roll
+    ('s defused  up inactive  s release', 'up inactive',
      type_(ec.KEY_Z)),
+    # end of s
     ('s defused (.*?) s release', r'\1', None),
 
-    # t-rocks
+    # t rocks
+    ('t (press|defused)  a press  a release', 't defused',
+     type_(ec.KEY_TAB)),
+    ('t (press|defused)  r press  r release', 't defused',
+     type_(ec.KEY_K)),
     ('t (press|defused)  s press  s release', 't defused',
-     type_(ec.KEY_P)),
+     type_(ec.KEY_B)),
     ('t (press|defused)  n press  n release', 't defused',
      type_(ec.KEY_T, ec.KEY_H)),
     ('t (press|defused)  e press  e release', 't defused',
      type_(ec.KEY_T, ec.KEY_H, ec.KEY_E)),
+    # end of t
     ('t defused (.*?) t release', r'\1', None),
 
-    # n-rocks
+    # n rocks
     ('n (press|defused)  a press  a release', 'n defused',
      type_(ec.KEY_H, ec.KEY_A)),
     ('n (press|defused)  t press  t release', 'n defused',
-     type_(ec.KEY_N, ec.KEY_D, ec.KEY_SPACE)),
+     type_(ec.KEY_N, ec.KEY_D)),
     ('n (press|defused)  e press  e release', 'n defused',
      type_(ec.KEY_H, ec.KEY_E)),
+    # end of n
     ('n defused (.*?) n release', r'\1', None),
 
-    # e-rocks
+    # e rocks
+    ('e (press|defused)  r press  r release', 'e defused',
+     type_(ec.KEY_E, ec.KEY_L)),
     ('e (press|defused)  t press  t release', 'e defused',
-     type_(ec.KEY_E, ec.KEY_D, ec.KEY_SPACE)),
+     type_(ec.KEY_E, ec.KEY_D)),
     ('e (press|defused)  n press  n release', 'e defused',
      type_(ec.KEY_H)),
+    ('e (press|defused)  i press  i release', 'e defused',
+     type_(ec.KEY_DOT)),
+    ('e (press|defused)  o press  o release', 'e defused',
+     type_(ec.KEY_APOSTROPHE)),
+    # e same-finger
+    ('e (press|defused)  left press', 'e defused  left inactive', None),
+    ('e defused  left inactive (.*) (e|left) repeat',
+     r'e defused  left inactive \1', None),
+    # e same-finger rock
+    ('e defused  left inactive  left release', 'e defused',
+     type_(ec.KEY_U)),
+    # e same-finger roll
+    ('e defused  left inactive  e release', 'left inactive',
+     type_(ec.KEY_U)),  # TODO: assign something different
+    # end of e
     ('e defused (.*?) e release', r'\1', None),
 
-    # i-rocks
+    # i rocks
     ('i (press|defused)  n press  n release', 'i defused',
-     type_(ec.KEY_I, ec.KEY_N, ec.KEY_G)),
+     type_(ec.KEY_I, ec.KEY_N, ec.KEY_G, ec.KEY_SPACE)),
     ('i (press|defused)  e press  e release', 'i defused',
-     type_(ec.KEY_U)),
-    ('i (press|defused)  right press', 'i defused  right inactive',
+     type_(ec.KEY_MINUS)),
+    ('i (press|defused)  o press  o release', 'i defused',
+     shifted(ec.KEY_SEMICOLON)),
+    # i same-finger
+    ('i (press|defused)  right press', 'i defused  right inactive', None),
+    ('i defused  right inactive (.*) (i|right) repeat',
+     r'i defused  right inactive \1', None),
+    # i same-finger rock
+    ('i defused  right inactive  right release', 'i defused',
+     type_(ec.KEY_Y)),
+    # i same-finger roll
+    ('i defused  right inactive  i release', 'right inactive',
      type_(ec.KEY_J)),
+    # end of i
     ('i defused (.*?) i release', r'\1', None),
 
-    # o-rocks
+    # o rocks
+    ('o (press|defused)  r press  r release', 'o defused',
+     type_(ec.KEY_O, ec.KEY_L)),
+    ('o (press|defused)  t press  t release', 'o defused',
+     type_(ec.KEY_O, ec.KEY_U, ec.KEY_L, ec.KEY_D, ec.KEY_SPACE)),
     ('o (press|defused)  e press  e release', 'o defused',
      type_(ec.KEY_O, ec.KEY_U)),
+    ('o (press|defused)  i press  i release', 'o defused',
+     type_(ec.KEY_ESC)),
+    # end of o
     ('o defused (.*?) o release', r'\1', None),
 
     # arst neio
@@ -138,6 +220,10 @@ RULES = (
      r'lthumb defused  rthumb defused \1', type_(ec.KEY_ENTER)),
     ('lthumb defused  rthumb defused (.*?) lthumb repeat',
      r'lthumb defused  rthumb defused \1', None),
+
+    # more on thumbs
+    ('lthumb (press|defused) (.*?) lthumb repeat',
+     r'lthumb defused \2', None),
 
     ('lthumb defused  rthumb defused (.*?) rthumb release',
      r'lthumb defused \1', None),
